896f800cee314b2117417f3c3d07dca14d49de85
TheLurkiest <bng5@wildcats.unh.edu>
2019-04-26 22:25:32 -0400

    reorganized our files so that the central_hub.py file would be the center from which all commands would be issued and info returned to

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # comp525_numpy_project
+changed central focus of the code to be based around the module called startup_menu_central_hub
--- colorcoded_topo_map_test.py
+++ colorcoded_topo_map_test.py
@@ -1,18 +1,14 @@
 import numpy as np 
 from skimage import io
 import matplotlib.pyplot as plt
-a1=io.imread('ancient_maya.jpg')
-
+a1=io.imread('lidar_color_code.jpg')
 
 d1={}
 
 # create empty list
-
 l_trunc=[]
 for row in a1:               
     l_trunc.append(list(row))
-
-
 
 for l_t_bit in l_trunc:
     for row in l_t_bit:
@@ -20,9 +16,7 @@ for l_t_bit in l_trunc:
                     d1[tuple(row)]=d1[tuple(row)]+1
             except:
                     d1[tuple(row)]=1
-
 #=========================================================================
-
 
 def key_sorter(l_in):
     return l_in[1]
@@ -37,10 +31,7 @@ l_output = []
 for l_step in l_midstep2:
     l_output.append(l_step[0])
 
-
 l_output.reverse()
-
-
 
 #=========================================================================
 p_reply=''
@@ -56,35 +47,35 @@ safety_valve_debug_counter=0
 for l_o_bit in l_output:
 	# breaks for debugging to avoid crazy loop errors if mistakes happen here now
 	# while under testing:
-	if(len(l_sorted2)>=10):
+	if(len(l_sorted2)>=21):
 		print('break here to debug')
 		break
-	if(safety_valve_debug_counter<=80):
+	if(safety_valve_debug_counter<=1080):
 		safety_valve_debug_counter=safety_valve_debug_counter+1
 	else:
 		print('break here to debug')
 		break
+	safety_valve_debug_counter=safety_valve_debug_counter+1
 	# debugs concluded!
+
+	c_deviation = 7
 
 	if(len(l_sorted2)==0):
 		l_sorted2.append(l_o_bit)
 	else:
 		for l_s_bit in l_sorted2:
-			if(abs(l_o_bit[0] - l_s_bit[0]) <= 5):
-				if(abs(l_o_bit[1] - l_s_bit[1]) <= 5):
-					if(abs(l_o_bit[2] - l_s_bit[2]) <= 5):
+			if(abs(l_o_bit[0] - l_s_bit[0]) <= c_deviation):
+				if(abs(l_o_bit[1] - l_s_bit[1]) <= c_deviation):
+					if(abs(l_o_bit[2] - l_s_bit[2]) <= c_deviation):
 						new_color=False
 		if(new_color==True):
 			l_sorted2.append(l_o_bit)
-			d1[l_o_bit] = d1[l_o_bit] + d1[l_s_bit]
+			# d1[l_o_bit] = d1[l_o_bit] + d1[l_s_bit]
 			del d1[l_s_bit]
 		else:
 			new_color=True
 
-
-
-
-print('here are is the long list or most-common colors that we have produced: '+str(l_sorted2))
+print('the list of most-common colors produced: '+str(l_sorted2))
 
 
 
