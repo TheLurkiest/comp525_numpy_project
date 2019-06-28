0f62fa61f9be6de2c90a9ea119a413aae0713fc7
TheLurkiest <bng5@wildcats.unh.edu>
2019-06-28 10:29:56 -0400

    cleaned up a little bit more

--- combo_simp_color_change.py
+++ combo_simp_color_change.py
@@ -27,7 +27,6 @@ a1 = io.imread('venusaur.png')
 plt.imshow(a1)
 plt.show()
 
-
 # this makes our image array coarser and therefore, quicker to perform changes to-- comment/uncomment this statement below to make things quicker or more detailed/slower-- or increase/decrease the numbers ::5, ::5 to fine-tune this feature:
 #a1=a1[::5,::5]
 
@@ -41,9 +40,6 @@ while(p_reply<0 or p_reply>2):
     p_reply = input('enter 0, 1, or 2 to alter how red, green or blue the photo is, respectively: ')
     p_reply = int(p_reply)
 
-
-
-
 color_highlighted=p_reply
 color_b=0
 color_c=0
@@ -51,53 +47,20 @@ color_c=0
 if(color_highlighted == 0):
     color_b=1
     color_c=2
-
-
-
 if(color_highlighted == 1):
     color_b=0
     color_c=2
-
-
-
-
-
 if(color_highlighted == 2):
     color_b=0
     color_c=1
 
-
-
-
-
-
 color_selection=int(p_reply)
 
-# XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-
-#print('ok this should display a CLEAN, UNALTERED IMAGE NOW as well: ')
-
-#a1 = io.imread('2019-03-04-042807_4.jpg')
-#a1 = io.imread('fing111.jpg')
-
-#p_reply=input('enter a number representing how much greater the the color you are searching for should be in proportion to the other two in the rgb color spectrum to be above the threshhold needed to cause our code to highlight it in the resulting image-- if unsure, the best default values for most images range from around 0 to 25: ')
-
-#color_prop_buffer=int(p_reply)
-
-print('for our purposes, rgb color choices are represented by 0, 1 and 2, respectively, representing their specific index.  For example, to select RED as an answer to one of these prompts therefore, you must enter 0 instead.  The prompts and printed statements will also refer to RED as 0 when asking the user questions.')
-
-print('we now ask questions pertaining to how much greater the color you wish to highlight is compared to each of the other two.  Choosing numbers between 0 and 25 is a good rule of thumb if unsure.  However, often you may find that choosing numbers here closer to 40 or 50 may be ideal.')
-
-p_reply=input('enter how much greater you want the color you are searching for to be compared to the color in index '+ str(['red','green','blue'][color_b]) + ' within rgb.  ')
+p_reply=input('the color we are searching for should be proportionally higher then '+ str(['red','green','blue'][color_b]) + ' by a value of at least this much more: ')
 color_prop_buffer_b=int(p_reply)
 
-p_reply=input('enter how much greater you want the color you are searching for to be compared to the color in index ' + str(['red','green','blue'][color_c]) + ' within rgb.  ')
+p_reply=input('the color we are searching for should be proportionally higher then '+ str(['red','green','blue'][color_c]) + ' by a value of at least this much more: ')
 color_prop_buffer_c=int(p_reply)
-
-
-print('...now for the altered image: ')
-
-print('testing out the X attempt now...')
 
 boo_thresh_high_b = a1[:,:,color_highlighted] > a1[:,:,color_b] + color_prop_buffer_b
 boo_thresh_high_c = a1[:,:,color_highlighted] > a1[:,:,color_c] + color_prop_buffer_c
@@ -144,6 +107,7 @@ rgb_swap_pic[:,:,2][booling2] = 125
 plt.imshow(rgb_swap_pic)
 plt.show()
 
+imsave('green_alt.png', rgb_swap_pic)
 
 
 # now to make things RED:
@@ -162,6 +126,18 @@ plt.show()
 imsave('red_alt.png', rgb_swap_pic)
 
 
+print('now to make our image have the portioned that were analyzed as being greater than the threshhold buffer value chosen in proportion to the other colors in the rgb spectrum not chosen have be HIGHLIGHTED BRIGHT BLUE to see these segments more clearly against the background:')
+
+rgb_swap_pic[:,:,0][booling2] = 125
+
+rgb_swap_pic[:,:,1][booling2] = 125
+
+rgb_swap_pic[:,:,2][booling2] = 255
+
+plt.imshow(rgb_swap_pic)
+plt.show()
+
+imsave('blue_alt.png', rgb_swap_pic)
 
 
 
@@ -207,8 +183,8 @@ imsave('highlighted_color_only.png', h_only)
 
 #------------------------------------------------------------------------
 
-
-im=imageio.imread('2019-03-04-042807_5.jpg')
+im=rgb_swap_pic
+#im=imageio.imread('2019-03-04-042807_5.jpg')
 
 
 plt.imshow(im)
